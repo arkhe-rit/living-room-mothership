e08979986a36e5e6d1d41f51232de9dc54fea233
cajunwildcat <elicheramie89@gmail.com>
2023-07-17 13:33:24 -0400

    removed unnecessary escape character

--- src/toolbox/wildcardComparison.js
+++ src/toolbox/wildcardComparison.js
@@ -1,6 +1,6 @@
 const wildcardComparison = (wildcardChannel, regularChannel) => {
     //format wildcard channel regex
-    let expression = wildcardChannel.replace('*', '.+').replace('/', '\/');
+    let expression = wildcardChannel.replace('*', '.+');
     expression = `^${expression}$`;
     const regex = new RegExp(expression);
     return regex.test(regularChannel);
